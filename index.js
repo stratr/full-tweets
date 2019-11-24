568cf63961b935c8939505fec4b6c9ae1503edfa
Taneli <taneli.salonen@gmail.com>
2019-11-24 13:52:37 +0200

    Read document title of tweet url

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ const axios = require("axios")
 
 const fetchData = async (siteUrl) => {
     const result = await axios.get(siteUrl)
-    console.log(result.data.indexOf('Vihreää'))
+    //console.log(result.data.indexOf('Mistä seuraavaksi'))
+    //console.log(result.data.slice(4000, 5000))
     return cheerio.load(result.data)
 };
 
 const getText = async () => {
     const $ = await fetchData("https://twitter.com/i/web/status/1189845865671909376")
-    const spanElements = $('article span')
+    const documentTitle = $('title').text()
+
+    const start = documentTitle.indexOf('"')
+    const end = documentTitle.lastIndexOf('"')
     
-    console.log(spanElements)
+    console.log(documentTitle.slice(start, end))
 }
 
 getText()
